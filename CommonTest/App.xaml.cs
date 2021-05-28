bff6a7e9171a3974ca767725a4fd4acb0d1cbe0d
潘文博 <964012840@qq.com>
2021-05-28 13:50:48 +0800

    添加项目文件。

---
+++ CommonTest/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows;
+
+using CommonTest.ThreadTest;
+
+namespace CommonTest
+{
+    /// <summary> Interaction logic for App.xaml </summary>
+    public partial class App  :Application
+    {
+
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            CPlusPlusInteropTest.InteropTest.PinvokeTest();
+
+            ManualResetEventTest.ThreadTest();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleManager.Show();
+            Console.WriteLine("在wpf中显示控制台");
+
+
+            ManualResetEventTest.ThreadTest();
+            ConsoleManager.Hide();
+        }
+    }
+}
---
+++ CommonTest/CPlusPlusInteropTest/InteropTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace CommonTest.CPlusPlusInteropTest
+{
+  public  class InteropTest
+    {
+
+        // Define a delegate that corresponds to the unmanaged function.
+        public delegate bool EnumWC(IntPtr hwnd, IntPtr lParam);
+
+        // Import user32.dll (containing the function we need) and define the method corresponding to the native function.
+        [DllImport("user32.dll")]
+        public static extern int EnumWindows(EnumWC lpEnumFunc, IntPtr lParam);
+
+        // Define the implementation of the delegate; here, we simply output the window handle.
+        public static bool OutputWindow(IntPtr hwnd, IntPtr lParam)
+        {
+            Console.WriteLine(hwnd.ToInt64());
+            return true;
+        }
+
+
+        // Invoke the method; note the delegate as a first parameter.
+        //CSharpCPlusPlusInterop. EnumWindows(CSharpCPlusPlusInterop.OutputWindow, IntPtr.Zero);
+        public static void PinvokeTest()
+        {
+            PInvoke.User32.EnumWindows(OutputWindow, IntPtr.Zero);
+        }
+    }
+
+      
+}
